b2351064c5cf2c41dd8842826ae311c550e2cea0
vaqanzel <valengrehasta@gmail.com>
2023-10-12 19:53:36 +0700

    first commit jobsheet 7

--- ForKelipatan28.java
+++ ForKelipatan28.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ForKelipatan28 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int kelipatan;
+        int kelipatan, rataRata;
         int jumlah = 0;
         int counter = 0;
         int total = 0;
@@ -11,14 +11,14 @@ public class ForKelipatan28 {
         kelipatan = scan.nextInt();
         for (int i = 1; i <= 50; i++){
             if (i % kelipatan == 0){
-                total += i;
+                total +=i;
                 counter++;
-                System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-                System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
             }
-
         }
-
+        rataRata = jumlah / counter;
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("Rata rata bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, rataRata);
     }
     
 }
