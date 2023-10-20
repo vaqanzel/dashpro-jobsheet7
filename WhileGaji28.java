8c81096f60a8770877d01d6fca6ed93e05dff188
vaqanzel <valengrehasta@gmail.com>
2023-10-20 09:49:13 +0700

    modifikasi untuk menghandle jabatan yang invalid

--- WhileGaji28.java
+++ WhileGaji28.java
@@ -1,33 +1,38 @@
 import java.util.Scanner;
 public class WhileGaji28 {
     
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner scan = new Scanner (System.in);
         int jumlahKaryawan, jumlahJamLembur;
         double gajiLembur = 0;
         double totalGajiLembur = 0;
         String jabatan;
+
         System.out.print("Masukkan jumlah karyawan : ");
         jumlahKaryawan = scan.nextInt();
+
         int i = 0;
         while (i < jumlahKaryawan) {
-            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan, Pegawai");
             System.out.print("Masukkan jabatan karyawan ke-" + (i+1)+ ": ");
             jabatan = scan.next();
             System.out.print("Masukkan jumlah jam lembur : ");
             jumlahJamLembur = scan.nextInt();
-            i++;
 
             if (jabatan.equalsIgnoreCase("direktur")){
+                gajiLembur = 0 * jumlahJamLembur;
+            } else if (jabatan.equalsIgnoreCase("pegawai")){
+                System.out.println("Jabatan invalid");
                 continue;
             } else if (jabatan.equalsIgnoreCase("manajer")){
                 gajiLembur = jumlahJamLembur * 100000;
-            }
-            else if (jabatan.equalsIgnoreCase("karyawan")) {
+            } else if (jabatan.equalsIgnoreCase("karyawan")) {
                 gajiLembur = jumlahJamLembur * 75000;
             }
             totalGajiLembur += gajiLembur;
-            System.out.println("Total gaji lembur :" + totalGajiLembur);
+            i++;
         }
+            System.out.println("Total gaji lembur :" + totalGajiLembur);
     }
+
 }
